e4e314e50eb84d51cbf5f0c05331fc72300d4978
Egor Zubachenko <egyabig2@gmail.com>
2025-05-21 13:51:18 +0700

    default styles core/commin -> ui/theme

--- lib/src/features/registration/registration_page.dart
+++ lib/src/features/registration/registration_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:tap_map/core/common/styles.dart';
+import 'package:tap_map/ui/theme/OLD_app_text_styles.dart';
 import 'package:tap_map/router/routes.dart';
 import 'package:tap_map/src/features/registration/bloc/registration_bloc.dart';
 import 'package:tap_map/src/widget/custom_elevated_button.dart';
