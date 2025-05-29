e34e40e4f5ee88759b440fa02ec76b909edc572d
Joe Kaseq <egyabig2@gmail.com>
2025-05-29 17:20:45 +0700

    NotificationService init handle errors & fix Firebase init iOS (#14)
    * fix error init Firebase for iOS
    
    * NotificationService init handle errors

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,6 +2,8 @@ import Flutter
 import UIKit
 import MapboxMaps
 import Firebase
+import FirebaseMessaging
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -9,8 +11,44 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {   
     GeneratedPluginRegistrant.register(with: self)
-    FirebaseApp.configure()
-    UNUserNotificationCenter.current().delegate = self
+    // TODO чекать норм ли что тут убрали инит и удалить через пару версий
+    // FirebaseApp.configure() - удалено, чтобы избежать двойной инициализации
+    
+    // Регистрация для push-уведомлений
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: { _, _ in }
+      )
+    } else {
+      let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+    
+    application.registerForRemoteNotifications()
+    
+    // Настройка Firebase Messaging
+    Messaging.messaging().delegate = self
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-} 
+  
+  // Получаем APNS токен и передаем его в Firebase Messaging
+  override func application(_ application: UIApplication,
+                           didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    Messaging.messaging().apnsToken = deviceToken
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+}
+
+// MARK: - MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    print("FCM registration token: \(String(describing: fcmToken))")
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
+  }
+}
