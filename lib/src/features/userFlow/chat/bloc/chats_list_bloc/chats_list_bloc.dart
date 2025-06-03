596d1057e3dcb152d3aa8a9e05df206f026b37bf
Joe Kaseq <egyabig2@gmail.com>
2025-06-03 18:06:54 +0700

    390 | chats v0.2.14: update ChatList via WebSocket (#41)

--- lib/src/features/userFlow/chat/bloc/chats_list_bloc/chats_list_bloc.dart
+++ lib/src/features/userFlow/chat/bloc/chats_list_bloc/chats_list_bloc.dart
@@ -1,11 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tap_map/src/features/userFlow/chat/bloc/chats_list_bloc/chats_list_event.dart';
 import 'package:tap_map/src/features/userFlow/chat/bloc/chats_list_bloc/chats_list_state.dart';
 import 'package:tap_map/src/features/userFlow/chat/data/chat_repository.dart';
+import 'package:tap_map/src/features/userFlow/chat/services/chat_websocket_service.dart';
+import '../../models/chat_model.dart';
 
 /// Блок для управления списком чатов
 class ChatsListBloc extends Bloc<ChatsListEvent, ChatsListState> {
   final ChatRepository _chatRepository;
+  StreamSubscription<List<ChatModel>>? _chatsSub;
+  StreamSubscription<WebSocketEventData>? _wsSub;
 
   ChatsListBloc({
     required ChatRepository chatRepository,
@@ -13,6 +19,17 @@ class ChatsListBloc extends Bloc<ChatsListEvent, ChatsListState> {
         super(ChatsListInitial()) {
     on<FetchChatsListEvent>(_onFetchChats);
     on<RefreshChatsListEvent>(_onRefreshChats);
+    on<ChatsUpdatedEvent>(_onChatsUpdated);
+
+    _chatsSub = _chatRepository.watchChats().listen(
+      (chats) => add(ChatsUpdatedEvent(chats)),
+    );
+
+    _wsSub = _chatRepository.webSocketEvents.listen((event) {
+      if (event.type == WebSocketEventType.message && event.data != null) {
+        _chatRepository.processWebSocketMessage(event.data!);
+      }
+    });
   }
 
   /// Обработчик события загрузки списка чатов
@@ -41,5 +58,19 @@ class ChatsListBloc extends Bloc<ChatsListEvent, ChatsListState> {
     } catch (e) {
       emit(ChatsListError('Не удалось обновить чаты: ${e.toString()}'));
     }
+  }
+
+  void _onChatsUpdated(
+    ChatsUpdatedEvent event,
+    Emitter<ChatsListState> emit,
+  ) {
+    emit(ChatsListLoaded(event.chats));
+  }
+
+  @override
+  Future<void> close() {
+    _chatsSub?.cancel();
+    _wsSub?.cancel();
+    return super.close();
   }
 }
