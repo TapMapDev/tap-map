6c4dcd21a439b7bfac0ac3e12f9d7eafa4085679
Joe Kaseq <egyabig2@gmail.com>
2025-06-04 15:12:15 +0700

    revert deleting TODOs

--- lib/src/features/userFlow/chat/data/remote/remote_chat_data_source.dart
+++ lib/src/features/userFlow/chat/data/remote/remote_chat_data_source.dart
@@ -340,6 +340,7 @@ class RemoteChatDataSource implements ChatDataSource {
         }
       }
 
+      // TODO remove later after test
       // Если не удалось получить из нового формата, попробуем использовать старый эндпоинт
       final fallbackResponse = await _dioClient.client.get('/chat/$chatId/messages/pinned/');
       
@@ -359,6 +360,7 @@ class RemoteChatDataSource implements ChatDataSource {
         print('❌ RemoteChatDataSource: Сервер вернул код ${fallbackResponse.statusCode} при запросе закрепленных сообщений');
         return [];
       }
+      // TODO remove
     } catch (e) {
       print('❌ RemoteChatDataSource: Ошибка при получении закрепленных сообщений: $e');
       return [];
